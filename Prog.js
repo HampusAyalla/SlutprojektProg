7468fe62d81410ea7561f705c35921320b984c7f
HampusAyalla <hampus.melinosterlindh@skola.taby.se>
2025-05-25 17:10:12 +0200

    fixat placeholder bilder, samt städat upp koden lite.

--- Prog.js
+++ Prog.js
@@ -1,5 +1,3 @@
-import platform from 'img/oak_woods_tileset'
-console.log(platform)
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
@@ -7,11 +5,18 @@ canvas.width = 1024
 canvas.height = 526
 
 const gravity = 0.25
+
+function createImage(imageSrc) {
+    const image = new Image()
+    image.src = imageSrc
+    return image
+}
+
 class Player {
-    constructor(){
+    constructor() {
         this.position = {
             x: 100,
-            y: 100            
+            y: 100
         }
         this.velocity = {
             x: 0,
@@ -20,11 +25,11 @@ class Player {
         this.width = 30
         this.height = 30
     }
-    draw(){
+    draw() {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
-    update(){
+    update() {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -36,128 +41,173 @@ class Player {
 }
 
 class Platform {
-    constructor({ x, y }){
+    constructor({ x, y, image }) {
         this.position = {
             x,
             y
         }
-        this.width = 500
-        this.height = 60
+        this.image = image
+        this.width = image.width
+        this.height = image.height
     }
 
-    draw(){
-        c.fillStyle = "blue"
-        c.fillRect(this.position.x,this.position.y,this.width,this.height)
-
-    }
-}
-
-const player = new Player()
-const platforms = [new Platform({x: -1, y: 470}),
-     new Platform({x: 350, y: 175}),
-     new Platform({x: 900, y: 200})]
-const keys = {
-    right: {
-        pressed: false
-    },
-    left: {
-        pressed: false 
-    },
-    up: {
-        pressed: false
-    },
-    down: {
-        pressed: false
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
-let scrollOffset = 0
+class GenericObject {
+    constructor({ x, y, image }) {
+        this.position = {
+            x,
+            y
+        }
+        this.image = image
+        this.width = image.width
+        this.height = image.height
+    }
 
-function animate(){
-    requestAnimationFrame(animate)
-    c.fillStyle = 'white'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    
-    platforms.forEach(platform =>{
-        platform.draw()
-    })
-    player.update()
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
 
-    if (keys.right.pressed && player.position.x < 400){
-        player.velocity.x = 5
-    } else if (keys.left.pressed && player.position.x > 100){
-        player.velocity.x = -5
-    } else {
-        player.velocity.x = 0
-        if (keys.right.pressed){
-            scrollOffset += 5
-            platforms.forEach(platform =>{
-                platform.position.x -= 5
-            })
-            
-        } else if (keys.left.pressed){
-            scrollOffset -= 5
-            platforms.forEach(platform =>{
-                platform.position.x += 5
-            })
-            
+const platformImage = createImage('./img/platform.png')
+const backgroundImage = createImage('./img/background.png')
+const hillsImage = createImage('./img/hills.png')
+
+platformImage.onload = () => {
+    const player = new Player()
+
+    const platforms = [
+        new Platform({ x: -1, y: 410, image: platformImage }),
+        new Platform({ x: platformImage.width - 3, y: 410, image: platformImage })
+    ]
+
+    const genericObject = [
+        new GenericObject({
+            x: -1,
+            y: -1,
+            image: backgroundImage
+        }),
+        new GenericObject({
+            x: -1,
+            y: -1,
+            image: hillsImage
+        })
+    ]
+
+
+    const keys = {
+        right: {
+            pressed: false
+        },
+        left: {
+            pressed: false
+        },
+        up: {
+            pressed: false
+        },
+        down: {
+            pressed: false
+        }
+    }
+
+    let scrollOffset = 0
+
+    function animate() {
+        requestAnimationFrame(animate)
+        c.fillStyle = 'white'
+        c.fillRect(0, 0, canvas.width, canvas.height)
+
+        genericObject.forEach(genericObject => {
+            genericObject.draw()
+        })
+
+        platforms.forEach(platform => {
+            platform.draw()
+        })
+
+        player.update()
+
+        if (keys.right.pressed && player.position.x < 400) {
+            player.velocity.x = 5
+        } else if (keys.left.pressed && player.position.x > 100) {
+            player.velocity.x = -5
+        } else {
+            player.velocity.x = 0
+            if (keys.right.pressed) {
+                scrollOffset += 5
+                platforms.forEach(platform => {
+                    platform.position.x -= 5
+                })
+                genericObject.forEach(genericObject => {
+                    genericObject.position.x -= 3
+                })
+            } else if (keys.left.pressed) {
+                scrollOffset -= 5
+                platforms.forEach(platform => {
+                    platform.position.x += 5
+                })
+                genericObject.forEach(genericObject => {
+                    genericObject.position.x += 3
+                })
+            }
         }
 
+        platforms.forEach(platform => {
+            if (
+                player.position.y + player.height <= platform.position.y &&
+                player.position.y + player.height + player.velocity.y >= platform.position.y &&
+                player.position.x + player.width >= platform.position.x &&
+                player.position.x <= platform.position.x + platform.width
+            ) {
+                player.velocity.y = 0
+            }
+        })
+
+        if (scrollOffset > 2000)
+            console.log("You Win")
     }
-platforms.forEach(platform =>{
-    if (player.position.y+player.height <= platform.position.y && player.position.y+player.height+player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width){
-        player.velocity.y = 0
+
+    animate()
+
+    addEventListener('keydown', ({ keyCode }) => {
+        switch (keyCode) {
+            case 65:
+                keys.left.pressed = true
+                break
+            case 68:
+                keys.right.pressed = true
+                break
+            case 87:
+                player.velocity.y -= 7.5
+                keys.up.pressed = true
+                break
+            case 83:
+                player.height = 20
+                keys.down.pressed = true
+                break
         }
     })
 
-    if(scrollOffset > 2000)
-        console.log("You Win")
+    addEventListener('keyup', ({ keyCode }) => {
+        switch (keyCode) {
+            case 65:
+                keys.left.pressed = false
+                break
+            case 68:
+                keys.right.pressed = false
+                break
+            case 87:
+                player.velocity.y -= 1
+                keys.up.pressed = false
+                break
+            case 83:
+                player.height = 30
+                player.position.y += 10
+                keys.down.pressed = false
+                break
+        }
+    })
 }
-
-animate()
-
-addEventListener('keydown', ({keyCode}) => {
-    switch (keyCode){
-        case 65:
-            console.log('left')
-            keys.left.pressed = true
-            break
-        case 68:
-            console.log('right')
-            keys.right.pressed = true
-            break
-        case 87:
-            console.log('up')
-            player.velocity.y -= 7.5
-            keys.up.pressed = true
-            break
-        case 83:
-            console.log('down')
-            player.height = 20
-            keys.down.pressed = true
-            break
-    }
-})
-addEventListener('keyup', ({keyCode}) => {
-    switch (keyCode){
-        case 65:
-            console.log('left')
-            keys.left.pressed = false
-            break
-        case 68:
-            console.log('right')
-            keys.right.pressed = false
-            break
-        case 87:
-            console.log('up')
-            player.velocity.y -= 1
-            keys.up.pressed = false
-            break
-        case 83:
-            console.log('down')
-            player.height = 30
-            player.position.y += 10
-            keys.down.pressed = false
-            break
-    }
-})
